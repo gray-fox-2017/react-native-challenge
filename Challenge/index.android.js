35a29786ef9a0389362dbee4627414192889f685
hakiemaul <noersedya@gmail.com>
2017-07-06 14:17:16 +0700

    implement react redux to weather

--- Challenge/index.android.js
+++ Challenge/index.android.js
@@ -1,8 +1,7 @@
 
 import React, { Component } from 'react'
 import {
-  AppRegistry,
-  View
+  AppRegistry
 } from 'react-native'
 import {
   StackNavigator
@@ -10,16 +9,6 @@ import {
 
 import Landing from './src/screens/Landing'
 import Weather from './src/screens/Weather'
-
-class Challenge extends Component {
-  render() {
-    return (
-      <View>
-        <Weather />
-      </View>
-    );
-  }
-}
 
 const App = StackNavigator({
   Landing: { screen: Landing },
